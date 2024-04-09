ec8f4ee127aca15145bfbb902cdc7efc526fc5ed
lbaumo <lucie.baumont@gmail.com>
2024-04-09 21:09:03 +0200

    function reorganization postage_stamp

--- shapepipe/modules/ngmix_package/postage_stamp.py
+++ shapepipe/modules/ngmix_package/postage_stamp.py
@@ -90,20 +90,23 @@ class Postage_stamp():
     Parameters
     ----------
     bkg_sub: bool, optional
-        True for background subtraction
+        ``True`` for background subtraction
     megacam_flip: bool, optional
-        True to flip megaprime coordinates with megapipe processing
+        ``True`` to flip megaprime coordinates with megapipe processing
     mask_frac: float
         maximum fraction of allowed masked pixels
     rescale_weights: bool
-        True rescales weights into variance maps
+        ``True`` rescales weights into variance maps
+    symmetrize_mask: bool
+        ``True`` will symmetrize mask     
     """
     def __init__(
         self,
         bkg_sub=True,
         megacam_flip=True,
         mask_frac=1/3.0,
-        rescale_weights=True
+        rescale_weights=True,
+        symmetrize_mask=False
 
     ):
         
@@ -111,6 +114,7 @@ class Postage_stamp():
         self.megacam_flip=megacam_flip
         self.mask_frac=mask_frac
         self.rescale_weights=rescale_weights
+        self.symmetrize_mask=symmetrize_mask
 
         self.gals = []
         self.psfs = []
@@ -118,129 +122,186 @@ class Postage_stamp():
         self.noise_ims = []
         self.flags = []
         self.jacobs = []
-        self.psf_sigma = []
 
-def preprocess_postage_stamps(self, vignet, tile_cat, obj_id):
-    """preprocess stamps.
+    def preprocess_postage_stamps(self, vignet, tile_cat, obj_id):
+        """preprocess stamps.
 
-    runs per object over all epochs.
-    requested preprocessing steps can include:
-        background subtraction
-        rescaling flux of individual epochs
-        flipping megacam vignets
-        remove objects that are masked more than mask_frac
-        converting a weight map into a variance map
+        runs per object over all epochs.
+        requested preprocessing steps can include:
+            background subtraction
+            rescaling flux of individual epochs
+            flipping megacam vignets
+            remove objects that are masked more than mask_frac
+            converting a weight map into a variance map
 
-    Parameters
-    ----------
-    vignet : Vignet
-        Array containing the postage stamp to flip
-    tile_cat : Tile_cat
-        coadded tile detection catalog
-    obj_id : int
-        ID of object in tile detection catalog
-    Returns
-    -------
-    postage_stamp
-        processed postage stamp object
-    """
+        Parameters
+        ----------
+        vignet : Vignet
+            Array containing the postage stamp to flip
+        tile_cat : Tile_cat
+            coadded tile detection catalog
+        obj_id : int
+            ID of object in tile detection catalog
+        Returns
+        -------
+        postage_stamp
+            processed postage stamp object
+        """
 
-    obj_id_det_cat = obj_id - 1
-    obj_id = str(obj_id)
+        obj_id_det_cat = obj_id - 1
+        obj_id = str(obj_id)
 
-    # raise error if psf or galaxy is missing
-    if (
-        (vignet.psf_cat[obj_id] == 'empty')
-        or (vignet.gal_cat[obj_id] == 'empty')
-    ):
-        raise AttributeError
+        # raise error if psf or galaxy is missing
+        if (
+            (vignet.psf_cat[obj_id] == 'empty')
+            or (vignet.gal_cat[obj_id] == 'empty')
+        ):
+            raise AttributeError
 
-    # define per-object lists of individual exposures to go into ngmix
-    stamp = Postage_stamp()
+        # define per-object lists of individual exposures to go into ngmix
+        stamp = Postage_stamp()
     
-    #identify exposure and ccd number from psf catalog 
-    epoch_list = list(vignet.psf_cat[obj_id].keys())
+        #identify exposure and ccd number from psf catalog 
+        epoch_list = list(vignet.psf_cat[obj_id].keys())
     
-    # process multi-epoch data
-    for exp_ccd in epoch_list:
-        exp_name, ccd_n_string = re.split('-', exp_ccd)
-        ccd_n = int(ccd_n_string)
+        # process multi-epoch data
+        for exp_ccd in epoch_list:
+            exp_name, ccd_n_string = re.split('-', exp_ccd)
+            ccd_n = int(ccd_n_string)
 
-        gal = (
-            vignet.gal_cat[obj_id][exp_ccd]['VIGNET']
-        )
-        # skip galaxies with zero image size (think about this)
-        if len(np.where(gal.ravel() == 0)[0]) != 0:
-            continue
-
-        # background subtract
-        if stamp.bkg_sub:
-            bkg = (
-                vignet.bkg_cat[obj_id][exp_ccd]['VIGNET']
+            gal = (
+                vignet.gal_cat[obj_id][exp_ccd]['VIGNET']
             )
-            gal_sub_bkg = _background_subtract(
-                gal,
-                bkg
+            # skip galaxies with zero image size (think about this)
+            if len(np.where(gal.ravel() == 0)[0]) != 0:
+                continue
+
+            # background subtract
+            if stamp.bkg_sub:
+                bkg = (
+                    vignet.bkg_cat[obj_id][exp_ccd]['VIGNET']
+                )
+                gal_sub_bkg = _background_subtract(
+                    gal,
+                    bkg
+                )
+            else:
+                gal_sub_bkg = gal
+
+            # flip tile megacam images to mactch ccd if necessary
+            if stamp.megacam_flip:
+                tile_vign = (
+                    _MegaCamFlip(np.copy(tile_vign[obj_id_det_cat]), ccd_n)
+                )
+
+            flag = (
+                vignet.flag_vign_cat[obj_id][exp_ccd]['VIGNET']
             )
-        else:
-            gal_sub_bkg = gal
+            flag[np.where(tile_vign == -1e30)] = 2**10
 
-        # filptile megacam images to mactch ccd if necessary
-        if stamp.megacam_flip:
-            tile_vign = (
-                _MegaCamFlip(np.copy(tile_vign[obj_id_det_cat]), ccd_n)
+            # remove objects masked greater than mask_frac
+            flag_tmp = flag.ravel()
+            masked = len(np.where(flag_tmp != 0)[0])
+            if  masked / (gal.shape[0] * gal.shape[1]) > self.mask_frac:
+                continue
+
+            # prepare weight map
+            weight_raw = (
+                vignet.weight_cat[obj_id][exp_ccd]['VIGNET']
             )
 
-        flag = (
-            vignet.flag_vign_cat[obj_id][exp_ccd]['VIGNET']
-        )
-        flag[np.where(tile_vign == -1e30)] = 2**10
-
-        # remove objects masked greater than mask_frac
-        flag_tmp = flag.ravel()
-        masked = len(np.where(flag_tmp != 0)[0])
-        if  masked / (gal.shape[0] * gal.shape[1]) > self.mask_frac:
-            continue
-
-        # prepare weight map
-        weight_raw = (
-            vignet.weight_cat[obj_id][exp_ccd]['VIGNET']
-        )
-
-        gal_masked, weight, noise_img = _prepare_ngmix_weights(
-            gal_sub_bkg,
-            weight_raw,
-            flag
-        )
+            gal_masked, weight, noise_img = self._prepare_ngmix_weights(
+                gal_sub_bkg,
+                weight_raw,
+                flag
+            )
    
-        jacob = _get_galsim_jacobian(
-            vignet.f_wcs_file[exp_name][ccd_n]['WCS'],
-            tile_cat.ra[obj_id_det_cat],
-            tile_cat.dec[obj_id_det_cat]
-        )
-
-        # rescale by relative zero-points
-        header = fits.Header.fromstring(
-            vignet.f_wcs_file[exp_name][ccd_n]['header']
-        )
-
-        gal_scaled, weight_scaled = _rescale_epoch_fluxes(
-            gal_masked,
-            weight,
-            header
+            jacob = _get_galsim_jacobian(
+                vignet.f_wcs_file[exp_name][ccd_n]['WCS'],
+                tile_cat.ra[obj_id_det_cat],
+                tile_cat.dec[obj_id_det_cat]
             )
 
-        # gather postage stamps in all of the epochs
-        stamp.gals.append(gal_scaled)
-        stamp.weights.append(weight_scaled)
-        stamp.flags.append(flag)
-        stamp.jacobs.append(jacob)
-        stamp.noise_ims.append(noise_img)
-        stamp.psfs.append(
-            vignet.psf_cat[obj_id][exp_ccd]['VIGNET']
-        )
-                
-    return stamp
+            # rescale by relative zero-points
+            header = fits.Header.fromstring(
+                vignet.f_wcs_file[exp_name][ccd_n]['header']
+            )
+
+            gal_scaled, weight_scaled = _rescale_epoch_fluxes(
+                gal_masked,
+                weight,
+                header
+                )
+
+            # gather postage stamps in all of the epochs
+            self.gals.append(gal_scaled)
+            self.weights.append(weight_scaled)
+            self.flags.append(flag)
+            self.jacobs.append(jacob)
+            self.noise_ims.append(noise_img)
+            self.psfs.append(
+                vignet.psf_cat[obj_id][exp_ccd]['VIGNET']
+            )
+
+    def _prepare_ngmix_weights(self, 
+        gal,    
+        weight,
+        flag,
+        bkground=None,
+        exp_ccd=None
+    ):
+        """Prepares weights for shape measurement.
+        bookkeeping for ngmix weights. runs on a single galaxy and epoch
+
+        Parameters
+        ----------
+        gal : numpy.ndarray
+            galaxy images. 
+        weight : numpy.ndarray
+            weight image 
+        flag : numpy.ndarray
+            flag image.    
+        bkground : numpy.array, optional
+            background image.
+        exp_ccd: str, optional
+            exposure-ccd combination, not currently implemented
+        Returns
+        -------
+        numpy.ndarray
+            galaxy image where noise replaces masked regions
+        numpy.ndarray
+            variance map for NGMIX
+        numpy.ndarray
+            noise image    
+
+        """ 
+
+        # integrate flag info into weights
+        weight_map = np.copy(weight)
+        if self.symmetrize_mask:
+            flag = _symmetrize_mask(flag)
+        
+        weight_map[np.where(flag != 0)] = 0.
+
+        if self.bkg_sub == True: 
+            sig_noise = _get_noise(bkground)    
+        else:
+            sig_noise = _sextractor_sky_background_dev(exp_ccd) 
+        
+        # create gaussian noise image for correlated noise correction
+        noise_img = self.rng.standard_normal(weight.shape) * sig_noise
+    
+        # fill in galaxy image masked regions with noise
+        noise_img_gal = self.rng.standard_normal(weight.shape) * sig_noise
+        gal_masked = np.copy(gal)
+        if (len(np.where(weight_map == 0)[0]) != 0):
+            gal_masked[weight_map == 0] = noise_img_gal[weight_map == 0]
+
+        # convert weight map to variance map
+        if self.rescale_weights == True:
+            weight_map *= 1 / sig_noise ** 2
+    
+        return gal_masked, weight_map, noise_img
 
 def _background_subtract(gal,bkg):
     """background subtraction.
@@ -263,7 +324,7 @@ def _background_subtract(gal,bkg):
 
     return gal_vign_sub_bkg
 
-def _MegaCamFlip(self, vign, ccd_nb):
+def _MegaCamFlip(vign, ccd_nb):
     """Flip for MegaCam.
 
     MegaPipe has CCDs that are upside down. This function flips the
@@ -282,7 +343,7 @@ def _MegaCamFlip(self, vign, ccd_nb):
     numpy.ndarray
         The flipped postage stamp
 
-    """
+    """   
     if ccd_nb < 18 or ccd_nb in [36, 37]:
         # swap x axis so origin is on top-right
         return np.rot90(vign, k=2)
@@ -292,7 +353,8 @@ def _MegaCamFlip(self, vign, ccd_nb):
         return vign
 
 def _rescale_epoch_fluxes(gal,weight,header):
-    """rescale epochs by relative zeropoints to be on the same flux scale
+    """rescale epoch fluxes.
+    rescale epochs by relative zeropoints to be on the same flux scale
         
     Parameters
     ----------
@@ -318,10 +380,9 @@ def _rescale_epoch_fluxes(gal,weight,header):
     return gal_scaled, weight_scaled
 
 def _get_galsim_jacobian(wcs, ra, dec):
-    """Get local wcs.
+    """Get galsim jacobian.
     This produces a galsim jacobian at a point.  We call it local_wcs because we convert to a ngmix object to create the jacobian later.
-    TO DO: can we do this within ngmix?
-
+    
     Parameters
     ----------
     wcs : astropy.wcs.WCS
@@ -389,70 +450,6 @@ def _symmetrize_mask(mask):
         mask[mask_zero] = 0.0
     
     return mask
-
-def _prepare_ngmix_weights(self, 
-    gal,    
-    weight,
-    flag,
-    symmetrize_mask=False,
-    bkground=None,
-    exp_ccd=None
-):
-    """Prepares weights for shape measurement.
-    bookkeeping for ngmix weights. runs on a single galaxy and epoch
-
-    Parameters
-    ----------
-    gal : numpy.ndarray
-        galaxy images. 
-    weight : numpy.ndarray
-        weight image 
-    flag : numpy.ndarray
-        flag image.    
-    symmetrize_mask : bool, optional
-        'True' if mask will be symmetrized; default is ``False``
-    bkground : numpy.array, optional
-        background image.
-    exp_ccd: str, optional
-        exposure-ccd combination, not currently implemented
-    Returns
-    -------
-    numpy.ndarray
-        galaxy image where noise replaces masked regions
-    numpy.ndarray
-        variance map for NGMIX
-    numpy.ndarray
-        noise image    
-
-    """ 
-
-    # integrate flag info into weights
-    weight_map = np.copy(weight)
-    if symmetrize_mask:
-        flag = _symmetrize_mask(flag)
-        
-    weight_map[np.where(flag != 0)] = 0.
-
-    if self.bkg_sub == True: 
-        sig_noise = _get_noise(bkground)    
-    else:
-        sig_noise = _sextractor_sky_background_dev(exp_ccd) 
-        
-    # create gaussian noise image for correlated noise correction
-    noise_img = self.rng.standard_normal(weight.shape) * sig_noise
-    
-    
-    # fill in galaxy image masked regions with noise
-    noise_img_gal = self.rng.standard_normal(weight.shape) * sig_noise
-    gal_masked = np.copy(gal)
-    if (len(np.where(weight_map == 0)[0]) != 0):
-        gal_masked[weight_map == 0] = noise_img_gal[weight_map == 0]
-
-    # convert weight map to variance map
-    if self.rescale_weights == True:
-        weight_map *= 1 / sig_noise ** 2
-    
-    return gal_masked, weight_map, noise_img
 
 def _sextractor_sky_background_dev(exp_ccd):
     #gets backdev from sextractor catalog

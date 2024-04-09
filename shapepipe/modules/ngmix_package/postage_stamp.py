e98ba85add4d45b50fdd6aa6e095c59fd39353d7
lbaumo <lucie.baumont@gmail.com>
2024-04-09 16:36:08 +0200

    random seed and symmetrized weights in postage_stamp

--- shapepipe/modules/ngmix_package/postage_stamp.py
+++ shapepipe/modules/ngmix_package/postage_stamp.py
@@ -7,95 +7,54 @@ from sqlitedict import SqliteDict
 from shapepipe.pipeline import file_io
 
 class Tile_cat():
-    """Postage stamps.
-    we have two main types of info- sextractor info and the vignet info. we want to store the sextractor stuff in an object so it is easy to access
-    I think we can ignore the tile cat stuff if we expand the fits reader, then it is an object we want to pass to the postage stamp maker
-
-    this creates postage stamps of objects to be processed along with requested preprocessing steps:
-    background subtraction
-    rescaling individual epochs
-    flipping megacam vignets if necessary
-    remove objects that are more than 1/3 masked
-    converting a weight map into a variance map by looking at the noise
+    """Tile cat.
+    SExtractor detection catalog from the tile. 
 
     Parameters
     ----------
-    paths to data, processing steps that we want
-    array of postage stamps, whih are themselves arrays of epochs
+    cat_path: str
+        path to detection catalog
 
     """
  
-def __init__(self, cat_path, bkg_sub, megacam_flip):
+def __init__(self, cat_path):
 
     self.cat_path = cat_path
-    self.bkg_sub = bkg_sub
-    self.megacam_flip = megacam_flip
-
-    # sextractor detection catalog for the tile
-    self.tile_vignet
-    dtype = [('obj_id','i4'),('ra','>f8'),('dec','>f8'),('flux','>f4'),('VIGNET', '>f4', (51, 51))]
-    #self.tile_data = np.recarray(())
     
 @classmethod
-def get_data(self, cat_path):
-    tile_cat = file_io.FITSCatalogue(
-            cat_path,
+def get_data(self):
+    cat = file_io.FITSCatalogue(
+            self.cat_path,
             SEx_catalogue=True,
         )
-    tile_cat.open()
-        # I would like to make this into an object cat
-    self.vign = np.copy(tile_cat.get_data()['VIGNET'])
+    cat.open()
+     
+    self.vign = np.copy(cat.get_data()['VIGNET'])
 
-    self.obj_id = np.copy(tile_cat.get_data()['NUMBER'])
-    self.ra = np.copy(tile_cat.get_data()['XWIN_WORLD'])
-    self.dec = np.copy(tile_cat.get_data()['YWIN_WORLD'])
-    self.flux = np.copy(tile_cat.get_data()['FLUX_AUTO'])
-    self.size = np.copy(tile_cat.get_data()['FWHM_WORLD'])
-    self.e = np.copy(tile_cat.get_data()['ELLIPTICITY'])
-    self.theta = np.copy(tile_cat.get_data()['THETA_WIN_WORLD'])
+    self.obj_id = np.copy(cat.get_data()['NUMBER'])
+    self.ra = np.copy(cat.get_data()['XWIN_WORLD'])
+    self.dec = np.copy(cat.get_data()['YWIN_WORLD'])
+    self.flux = np.copy(cat.get_data()['FLUX_AUTO'])
+    self.size = np.copy(cat.get_data()['FWHM_WORLD'])
+    #self.e = np.copy(cat.get_data()['ELLIPTICITY'])
+    #self.theta = np.copy(cat.get_data()['THETA_WIN_WORLD'])
 
-    tile_cat.close()
-
-# we want this to inherit properties of catalog
-class Postage_stamp():
-    """Galaxy Postage Stamp.
-
-    Class to hold catalog of postage stamps for a single galaxy
-
-    Parameters
-    ----------
-    bkg_sub: bool
-
-    megacam_flip: bool
-    We probably want to put weight and flag options here too
-
-    """
-    def __init__(
-        self,
-        bkg_sub=True,
-        megacam_flip=True
-
-    ):
-        self.gals = []
-        self.psfs = []
-        self.weights = []
-        self.flags = []
-        self.jacobs = []
-
+    cat.close()
 
 class Vignet():
     """Vignet.
 
-    Class to hold catalog of postage stamps
+    Class to hold SqliteDicts of vignettes
+    These will be compiled into postage stamps.
 
     Parameters
     ----------
-    gal_vignet_path
-    bkg_vignet_path
-    psf_vignet_path
-    weight_vignet_path
-    flag_vignet_path
-    f_wcs_path
+    gal_vignet_path: str
+    bkg_vignet_path: str
+    psf_vignet_path: str
+    weight_vignet_path: str
+    flag_vignet_path: str
+    f_wcs_path: str
     """
     def __init__(
         self,
@@ -108,139 +67,182 @@ class Vignet():
 
     ):
         self.f_wcs_file = SqliteDict(f_wcs_path)
-        self.gal_vign_cat = SqliteDict(gal_vignet_path)
-        self.bkg_vign_cat = SqliteDict(bkg_vignet_path)
-        self.psf_vign_cat = SqliteDict(psf_vignet_path)
-        self.weight_vign_cat = SqliteDict(weight_vignet_path)
-        self.flag_vign_cat = SqliteDict(flag_vignet_path)
+        self.gal_cat = SqliteDict(gal_vignet_path)
+        self.bkg_cat = SqliteDict(bkg_vignet_path)
+        self.psf_cat = SqliteDict(psf_vignet_path)
+        self.weight_cat = SqliteDict(weight_vignet_path)
+        self.flag_cat = SqliteDict(flag_vignet_path)
 
     @classmethod
     def close(self):
         self.f_wcs_file.close()
-        self.gal_vign_cat.close()
-        self.bkg_vign_cat.close()
-        self.flag_vign_cat.close()
-        self.weight_vign_cat.close()
-        self.psf_vign_cat.close()
+        self.gal_cat.close()
+        self.bkg_cat.close()
+        self.flag_cat.close()
+        self.weight_cat.close()
+        self.psf_cat.close()
 
-    def MegaCamFlip(self, vign, ccd_nb):
-        """Flip for MegaCam.
+class Postage_stamp():
+    """Galaxy Postage Stamp.
 
-        MegaPipe has CCDs that are upside down. This function flips the
-        postage stamps in these CCDs. TO DO: This will give incorrect results
-        when used with THELI ccds.  Fix this.
+    Class to hold postage stamps cutouts for a single galaxy
 
-        Parameters
-        ----------
-        vign : numpy.ndarray
-            Array containing the postage stamp to flip
-        ccd_nb : int
-            ID of the CCD containing the postage stamp
+    Parameters
+    ----------
+    bkg_sub: bool, optional
+        True for background subtraction
+    megacam_flip: bool, optional
+        True to flip megaprime coordinates with megapipe processing
+    mask_frac: float
+        maximum fraction of allowed masked pixels
+    rescale_weights: bool
+        True rescales weights into variance maps
+    """
+    def __init__(
+        self,
+        bkg_sub=True,
+        megacam_flip=True,
+        mask_frac=1/3.0,
+        rescale_weights=True
 
-        Returns
-        -------
-        numpy.ndarray
-            The flipped postage stamp
-
-        """
-        if ccd_nb < 18 or ccd_nb in [36, 37]:
-            # swap x axis so origin is on top-right
-            return np.rot90(vign, k=2)
-            print('rotating megapipe image')
-        else:
-            # swap y axis so origin is on bottom-left
-            return vign
+    ):
         
-def preprocess_postage_stamps(vignet, tile_cat):
-    i_tile = tile_cat.obj_id - 1
-    obj_id = tile_cat.obj_id
-    # define per-object lists of individual exposures to go into ngmix
-    stamp = Postage_stamp()
+        self.bkg_sub=bkg_sub
+        self.megacam_flip=megacam_flip
+        self.mask_frac=mask_frac
+        self.rescale_weights=rescale_weights
+
+        self.gals = []
+        self.psfs = []
+        self.weights = []
+        self.noise_ims = []
+        self.flags = []
+        self.jacobs = []
+        self.psf_sigma = []
+
+def preprocess_postage_stamps(self, vignet, tile_cat, obj_id):
+    """preprocess stamps.
+
+    runs per object over all epochs.
+    requested preprocessing steps can include:
+        background subtraction
+        rescaling flux of individual epochs
+        flipping megacam vignets
+        remove objects that are masked more than mask_frac
+        converting a weight map into a variance map
+
+    Parameters
+    ----------
+    vignet : Vignet
+        Array containing the postage stamp to flip
+    tile_cat : Tile_cat
+        coadded tile detection catalog
+    obj_id : int
+        ID of object in tile detection catalog
+    Returns
+    -------
+    postage_stamp
+        processed postage stamp object
+    """
+
+    obj_id_det_cat = obj_id - 1
+    obj_id = str(obj_id)
+
+    # raise error if psf or galaxy is missing
     if (
-        (vignet.psf_vign_cat[str(obj_id)] == 'empty')
-        or (vignet.gal_vign_cat[str(obj_id)] == 'empty')
+        (vignet.psf_cat[obj_id] == 'empty')
+        or (vignet.gal_cat[obj_id] == 'empty')
     ):
         raise AttributeError
-    #identify exposure and ccd number from psf catalog
-    psf_expccd_names = list(vignet.psf_vign_cat[str(obj_id)].keys())
-    for expccd_name in psf_expccd_names:
-        exp_name, ccd_n = re.split('-', expccd_name)
 
-        gal_vign = (
-            vignet.gal_vign_cat[str(obj_id)][expccd_name]['VIGNET']
+    # define per-object lists of individual exposures to go into ngmix
+    stamp = Postage_stamp()
+    
+    #identify exposure and ccd number from psf catalog 
+    epoch_list = list(vignet.psf_cat[obj_id].keys())
+    
+    # process multi-epoch data
+    for exp_ccd in epoch_list:
+        exp_name, ccd_n_string = re.split('-', exp_ccd)
+        ccd_n = int(ccd_n_string)
+
+        gal = (
+            vignet.gal_cat[obj_id][exp_ccd]['VIGNET']
         )
-
-        if len(np.where(gal_vign.ravel() == 0)[0]) != 0:
+        # skip galaxies with zero image size (think about this)
+        if len(np.where(gal.ravel() == 0)[0]) != 0:
             continue
-        
+
+        # background subtract
         if stamp.bkg_sub:
-            bkg_vign = (
-                vignet.bkg_vign_cat[str(obj_id)][expccd_name]['VIGNET']
+            bkg = (
+                vignet.bkg_cat[obj_id][exp_ccd]['VIGNET']
             )
-            gal_vign_sub_bkg = background_subtract(
-                gal_vign,
-                bkg_vign
+            gal_sub_bkg = _background_subtract(
+                gal,
+                bkg
             )
         else:
-            gal_vign_sub_bkg = gal_vign
+            gal_sub_bkg = gal
 
+        # filptile megacam images to mactch ccd if necessary
         if stamp.megacam_flip:
             tile_vign = (
-                MegaCamFlip(np.copy(tile_vign[i_tile]), int(ccd_n))
+                _MegaCamFlip(np.copy(tile_vign[obj_id_det_cat]), ccd_n)
             )
 
-        flag_vign = (
-            vignet.flag_vign_cat[str(obj_id)][expccd_name]['VIGNET']
+        flag = (
+            vignet.flag_vign_cat[obj_id][exp_ccd]['VIGNET']
         )
-        flag_vign[np.where(tile_vign == -1e30)] = 2**10
-        v_flag_tmp = flag_vign.ravel()
-        # remove objects that are more than 1/3 masked
-        if len(np.where(v_flag_tmp != 0)[0]) / (51 * 51) > 1 / 3.0:
+        flag[np.where(tile_vign == -1e30)] = 2**10
+
+        # remove objects masked greater than mask_frac
+        flag_tmp = flag.ravel()
+        masked = len(np.where(flag_tmp != 0)[0])
+        if  masked / (gal.shape[0] * gal.shape[1]) > self.mask_frac:
             continue
 
-        weight_vign = (
-            vignet.weight_vign_cat[str(obj_id)][expccd_name]['VIGNET']
+        # prepare weight map
+        weight_raw = (
+            vignet.weight_cat[obj_id][exp_ccd]['VIGNET']
         )
 
-            ################This should be done when we make the stamps
-        # prepare weight map
-        gal_masked, weight_map, noise_img = prepare_ngmix_weights(
-            gal,
-            weight,
+        gal_masked, weight, noise_img = _prepare_ngmix_weights(
+            gal_sub_bkg,
+            weight_raw,
             flag
         )
-        # WHY RECENTER???
    
-        jacob = get_galsim_jacobian(
-            vignet.f_wcs_file[exp_name][int(ccd_n)]['WCS'],
-            tile_cat.ra[i_tile],
-            tile_cat.dec[i_tile]
-        )
-
-        header = fits.Header.fromstring(
-            vignet.f_wcs_file[exp_name][int(ccd_n)]['header']
+        jacob = _get_galsim_jacobian(
+            vignet.f_wcs_file[exp_name][ccd_n]['WCS'],
+            tile_cat.ra[obj_id_det_cat],
+            tile_cat.dec[obj_id_det_cat]
         )
 
         # rescale by relative zero-points
-        gal_vign_scaled, weight_vign_scaled = rescale_epoch_fluxes(
-            gal_vign_sub_bkg,
-            weight_vign,
+        header = fits.Header.fromstring(
+            vignet.f_wcs_file[exp_name][ccd_n]['header']
+        )
+
+        gal_scaled, weight_scaled = _rescale_epoch_fluxes(
+            gal_masked,
+            weight,
             header
             )
 
         # gather postage stamps in all of the epochs
-        stamp.gal_vign_list.append(gal_vign_scaled)
-        stamp.psf_vign_list.append(
-            vignet.psf_vign_cat[str(obj_id)][expccd_name]['VIGNET']
+        stamp.gals.append(gal_scaled)
+        stamp.weights.append(weight_scaled)
+        stamp.flags.append(flag)
+        stamp.jacobs.append(jacob)
+        stamp.noise_ims.append(noise_img)
+        stamp.psfs.append(
+            vignet.psf_cat[obj_id][exp_ccd]['VIGNET']
         )
-
-        stamp.weight_vign_list.append(weight_vign_scaled)
-        stamp.flag_vign_list.append(flag_vign)
-        stamp.jacob_list.append(jacob)
                 
     return stamp
 
-def background_subtract(gal,bkg):
+def _background_subtract(gal,bkg):
     """background subtraction.
         
     Parameters
@@ -261,7 +263,35 @@ def background_subtract(gal,bkg):
 
     return gal_vign_sub_bkg
 
-def rescale_epoch_fluxes(gal,weight,header):
+def _MegaCamFlip(self, vign, ccd_nb):
+    """Flip for MegaCam.
+
+    MegaPipe has CCDs that are upside down. This function flips the
+    postage stamps in these CCDs. TO DO: This will give incorrect results
+    when used with THELI ccds.  Fix this.
+
+    Parameters
+    ----------
+    vign : numpy.ndarray
+        Array containing the postage stamp to flip
+    ccd_nb : int
+        ID of the CCD containing the postage stamp
+
+    Returns
+    -------
+    numpy.ndarray
+        The flipped postage stamp
+
+    """
+    if ccd_nb < 18 or ccd_nb in [36, 37]:
+        # swap x axis so origin is on top-right
+        return np.rot90(vign, k=2)
+        print('rotating megapipe image')
+    else:
+        # swap y axis so origin is on bottom-left
+        return vign
+
+def _rescale_epoch_fluxes(gal,weight,header):
     """rescale epochs by relative zeropoints to be on the same flux scale
         
     Parameters
@@ -287,7 +317,7 @@ def rescale_epoch_fluxes(gal,weight,header):
 
     return gal_scaled, weight_scaled
 
-def get_galsim_jacobian(wcs, ra, dec):
+def _get_galsim_jacobian(wcs, ra, dec):
     """Get local wcs.
     This produces a galsim jacobian at a point.  We call it local_wcs because we convert to a ngmix object to create the jacobian later.
     TO DO: can we do this within ngmix?
@@ -304,7 +334,7 @@ def get_galsim_jacobian(wcs, ra, dec):
     Returns
     -------
     galsim.wcs.BaseWCS.jacobian
-        Jacobian of the WCS at the required position
+        Jacobian of the WCS at the given position
 
     """
     g_wcs = galsim.fitswcs.AstropyWCS(wcs=wcs)
@@ -316,13 +346,13 @@ def get_galsim_jacobian(wcs, ra, dec):
 
     return galsim_jacob
 
-def get_noise(background_vignet):
+def _get_noise(bkg_array):
     """Get Noise.
-    Computes sigma of sky background from object postage stamp.
+    Computes median deviation of sky background from object postage stamp.
 
     Parameters
     ----------
-    background_vignet : numpy.ndarray
+    bkg_array : numpy.ndarray
         Sextractor sky background postage stamp
     
     Returns
@@ -330,34 +360,61 @@ def get_noise(background_vignet):
     float
         Sigma of the noise on the galaxy image
     """
-    sig_noise = sigma_mad(background_vignet)
+    sig_noise = sigma_mad(bkg_array)
 
     return sig_noise
 
-def prepare_ngmix_weights(
-    gal,
+def _symmetrize_mask(mask):
+    """Symmetrize masks.
+    Symmetrizes pixels in mask image adding 90 deg rotated mask
+    note: don't symmetrize the ubserseg. This effect is for bad columns, edges, bleeds
+
+    Parameters
+    ----------
+    mask : numpy.ndarray
+        weight image
+
+    Returns
+    -------
+    np.ndarray
+        symmetrized mask image
+
+    """
+    assert mask.shape[0] == mask.shape[1]
+
+    mask_rot=np.rot90(mask)
+    mask_zero = np.where(mask_rot == 0.0)
+
+    if mask_zero[0].size > 0:
+        mask[mask_zero] = 0.0
+    
+    return mask
+
+def _prepare_ngmix_weights(self, 
+    gal,    
     weight,
     flag,
-    weight_type='megapipe',
-    symmetrize_mask=False
+    symmetrize_mask=False,
+    bkground=None,
+    exp_ccd=None
 ):
-    """bookkeeping for ngmix weights. runs on a single galaxy and epoch
-        pixel scale and galaxy guess
-        TO DO: decide if we want galaxy guess stuff
+    """Prepares weights for shape measurement.
+    bookkeeping for ngmix weights. runs on a single galaxy and epoch
 
     Parameters
     ----------
     gal : numpy.ndarray
-        galaxy image.  List indices run over epochs
+        galaxy images. 
     weight : numpy.ndarray
-        weight image  List indices run over epochs
+        weight image 
     flag : numpy.ndarray
-        flag image.  List indices run over epochs   
-    weight_type : str, optional
-        'THELI' or 'megapipe' (default)
+        flag image.    
     symmetrize_mask : bool, optional
         'True' if mask will be symmetrized; default is ``False``
-
+    bkground : numpy.array, optional
+        background image.
+    exp_ccd: str, optional
+        exposure-ccd combination, not currently implemented
     Returns
     -------
     numpy.ndarray
@@ -368,71 +425,36 @@ def prepare_ngmix_weights(
         noise image    
 
     """ 
+
     # integrate flag info into weights
     weight_map = np.copy(weight)
-    #if symmetrize_mask:
-        #### add ask symmetrization here
+    if symmetrize_mask:
+        flag = _symmetrize_mask(flag)
+        
     weight_map[np.where(flag != 0)] = 0.
 
-    if weight_type == 'THELI':
-        sig_noise = sextractor_sky_background_dev(filename)
-    elif weight_type == 'megapipe':
-        sig_noise = sigma_mad(gal)
-        # note: we need to mask galaxy image here
-    #    sig_noise = get_noise(
-    #        gal,
-    #        weight,
-    #        gal_guess_tmp,
-    #        pixel_scale,
-    #    )
+    if self.bkg_sub == True: 
+        sig_noise = _get_noise(bkground)    
     else:
-        raise TypeError(f'weight_type must be THELI or megapipe')
-
+        sig_noise = _sextractor_sky_background_dev(exp_ccd) 
+        
     # create gaussian noise image for correlated noise correction
-    noise_img = np.random.randn(*gal.shape) * sig_noise
-    noise_img_gal = np.random.randn(*gal.shape) * sig_noise
+    noise_img = self.rng.standard_normal(weight.shape) * sig_noise
+    
     
     # fill in galaxy image masked regions with noise
+    noise_img_gal = self.rng.standard_normal(weight.shape) * sig_noise
     gal_masked = np.copy(gal)
     if (len(np.where(weight_map == 0)[0]) != 0):
         gal_masked[weight_map == 0] = noise_img_gal[weight_map == 0]
 
     # convert weight map to variance map
-    weight_map *= 1 / sig_noise ** 2
+    if self.rescale_weights == True:
+        weight_map *= 1 / sig_noise ** 2
     
     return gal_masked, weight_map, noise_img
 
-#def sextractor_sky_background_dev(filename):
+def _sextractor_sky_background_dev(exp_ccd):
     #gets backdev from sextractor catalog
-#    return backdev
-
-# Define the SExtractor parameters for a galaxy
-def sextractor_e1e2(e,theta):
-    """sextractor_e1e2
-
-    computes ellipticity from sextrator quantities
-    Parameters
-    ----------
-    stamp : Postage_stamp
-        List of the galaxy vignets.  List indices run over epochs
-    prior : ngmix.priors
-        Priors for the fitting parameters
-    flux_guess : np.ndarray
-        guess for flux
-    pixel_scale : float
-        pixel scale in arcsec
-    rng : numpy.random.RandomState
-        Random state for guesses and priors    
-
-    Returns
-    -------
-    np.ndarray
-        ellipticity
-
-    """
-    # Convert the position angle from degrees to radians
-    phi = np.radians(theta) - np.pi/2
-    # Calculate the ellipticity vector
-    e_vec = e * np.array([np.cos(2*phi), np.sin(2*phi)])
-    return e_vec
+    raise NotImplementedError("Sextractor backdev not implemented")
 
